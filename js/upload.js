f68cee086ca73f09a3bf4d6d78324da94cc1ff5c
dcohen25 <David_A_Cohen@Brown.edu>
2016-11-01 19:55:56 -0400

    sfd

--- js/upload.js
+++ js/upload.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 	var imageForm = document.getElementById("#image-form");
-	imageForm.on("submit", function(e){
+	imageForm.addEventListener("submit", function(e){
 		e.preventDefault();
 		var imageFile = document.getElementById("#image-form-file");
 		var text = OCRAD(imageFile[0]);

671cc3c36d9e5caac85e87538f77b0ff389f4b3b
dcohen25 <David_A_Cohen@Brown.edu>
2016-11-01 19:57:00 -0400

    df

--- js/upload.js
+++ js/upload.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-	var imageForm = document.getElementById("#image-form");
+	var imageForm = document.getElementById("image-form");
 	imageForm.addEventListener("submit", function(e){
 		e.preventDefault();
-		var imageFile = document.getElementById("#image-form-file");
+		var imageFile = document.getElementById("image-form-file");
 		var text = OCRAD(imageFile[0]);
 		console.log(text);
 	});

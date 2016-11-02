3f34c8ffaaa982cc29ad58178d0448f286068958
dcohen25 <David_A_Cohen@Brown.edu>
2016-11-01 20:13:00 -0400

    Update

--- js/upload.js
+++ js/upload.js
@@ -2,8 +2,18 @@ window.onload = function(){
 	var imageForm = document.getElementById("image-form");
 	imageForm.addEventListener("submit", function(e){
 		e.preventDefault();
-		var imageFile = document.getElementById("image-form-file");
-		var text = OCRAD(imageFile.files[0]);
-		console.log(text);
+		var reader = new FileReader();
+		reader.onload = function(){
+			var img = new Image();
+			img.onload = function(){
+				OCRAD(img, function(text){
+					var imageText = document.getElementById('image-text');
+					imageText.innerText = text;
+				});
+			}
+			img.src = reader.result;
+		}
+		var imageFile = document.getElementById('image-form-file');
+		reader.readAsDataURL(imageFile.files[0])
 	});
 }
